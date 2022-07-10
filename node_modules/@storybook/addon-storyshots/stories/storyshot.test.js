0db9547f3a71e8d7958c24ca3049514ec6e2a7e5
schroneko <schroneko.fuller@gmail.com>
2022-07-10 10:21:29 +0900

    rm: node_modules

--- node_modules/@storybook/addon-storyshots/stories/storyshot.test.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import initStoryshots, { multiSnapshotWithOptions } from '../src';
-
-// with react-test-renderer
-initStoryshots({
-  framework: 'react',
-  configPath: path.join(__dirname, '..', '.storybook'),
-  test: multiSnapshotWithOptions({}),
-});
